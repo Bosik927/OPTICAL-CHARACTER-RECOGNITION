c30f9ce753f02be3e8e5481cb38fade9b1b222d3
Stanisław Fiuta <stanislaw.fiuta@gmail.com>
2019-05-26 16:03:44 +0200

    Join pre processing and view

--- src/ViewInitializer.py
+++ src/ViewInitializer.py
@@ -2,6 +2,7 @@ import tkinter
 from tkinter import *
 from PIL import ImageTk, Image
 from tkinter.filedialog import askopenfilename
+import Preprocesing as pre
 import cv2
 
 picture_width = 960
@@ -9,88 +10,63 @@ picture_height = 700
 rectangles = []
 
 
-# def search_button_event(event):
-#     actual_path = askopenfilename()
-#     img = resize_image(actual_path)
-#     View.create_main_image(picture_width, picture_height, img)
-#     # createLeftImage(actual_path)
-#     print('dupa')
-#     print('dupa')
-#     # createGreyScaleImage(path)
-#     # createRightImage('greyscale.png')
-
-
-# Changing to binary picture
-# def createLeftImage(path):
-#     originalImage = Image.open(path)
-#     resizedImage = originalImage.resize((picture_width, picture_height), Image.ANTIALIAS)
-#     readImage = ImageTk.PhotoImage(resizedImage)
-#
-#     # main_image.image_on_canvas(image=readImage)
-#     main_image.create_image(0, 0, image=image, anchor=tkinter.NW)
-#
-#
-# def createGreyScaleImage(path):
-#     greyScaleImage = Image.open(path).convert('LA')
-#     convertedImage2 = ImageTk.PhotoImage(greyScaleImage)
-#     rightImage.configure(image=convertedImage2)
-#     rightImage.image = convertedImage2
-#     return greyScaleImage
-#
-#
-# def createRightImage(path):
-#     img = cv2.imread(path)
-#     im_bw = cv2.threshold(img, 128, 255, cv2.THRESH_BINARY)[1]
-#     cv2.imwrite('binary_image.png', im_bw)
-#
-#     binaryImage = Image.open('binary_image.png')
-#     rezisedImage = binaryImage.resize((picture_width, picture_height), Image.ANTIALIAS)
-#     binaryImage = ImageTk.PhotoImage(rezisedImage)
-#     rightImage.configure(image=binaryImage)
-#     rightImage.image = binaryImage
-#
-#
-# def searchButtonEvent(event):
-#     path = askopenfilename()
-#     createLeftImage(path)
-#     createGreyScaleImage(path)
-#     createRightImage('greyscale.png')
-
-
-# Creating rectangles
-# def disappearRectangle(event):
-#     rectangle.grid_forget()
-#
-#
-# def createRectangle(event):
-#     rectangle.grid(row=1, column=0)
-
-
 def create_searching_button(root, canvas, image_on_canvas):
     searching_button = tkinter.Button(root, text="Search image")
     searching_button.grid(columnspan=1)
-    searching_button.bind('<Button-1>', lambda event, can=canvas, img=image_on_canvas: search_button_event(can, img))
+    searching_button.bind('<Button-1>',
+                          lambda event, can=canvas, img=image_on_canvas, ro=root: search_button_event(can, img, ro))
     return searching_button
 
 
-def search_button_event(canvas, image_on_canvas):
+# TODO: Optimize
+def search_button_event(canvas, image_on_canvas, root):
     path = askopenfilename()
     image = resize_image(path)
-
     canvas.itemconfig(image_on_canvas, image=image)
+    canvas.itemconfig(image_on_canvas, image=image)
+
+    (grey_scale_image, gui_grey_scale_image) = create_grey_scale_image(path)
+    grey_scale_image.save('grey_scale_2.png')
+
+    img = cv2.imread('grey_scale_2.png')
+    words = pre.cropTextLine(img)
+
+    create_rectangles2(root, canvas, words)
+
+    img = cv2.imread('grey_scale_2.png')
+    im_bw = cv2.threshold(img, 128, 255, cv2.THRESH_BINARY)[1]
+    cv2.imwrite('binary_image2.png', im_bw)
+    create_binary_vector(im_bw)
+
     canvas.itemconfig(image=image)
 
-    # canvas.itemconfig(image_on_canvas, image=image)
-    # print(image.height())
-    # print(image.width())
 
-    # image_canvas = create_main_image(picture_width, picture_height, image)
+def create_rectangles2(root, canvas, words):
+    rectangles.clear()
+    for word in words:
+        rectangles.append(
+            create_rectangle(word.x1 - 3, word.y1 - 3, word.x2 + 3, word.y2 + 3, root, canvas, fill='green', alpha=.2))
 
-    # path = askopenfilename()
-    # create_image(path)
-    # print('search_button_event')
-    # createGreyScaleImage(path)
-    # createRightImage('greyscale.png')
+
+# TODO: Implement
+def create_binary_vector(img):
+    height, width, channels = img.shape
+    BLACK = [255, 255, 255]
+
+    for x in range(height):
+        for y in range(width):
+            asd = img[x, y]
+            if asd == BLACK:
+                print('1')
+            else:
+                print('0')
+    # print(str(height) + ' ' + str(width))
+
+
+def create_grey_scale_image(path):
+    grey_scale_image = Image.open(path).convert('LA')
+    gui_grey_scale_image = ImageTk.PhotoImage(grey_scale_image)
+    return grey_scale_image, gui_grey_scale_image
 
 
 # Changing to binary picture
@@ -124,20 +100,13 @@ def create_main_image(image, root):
 
     image_on_canvas = canvas.create_image(0, 0, image=image, anchor=tkinter.NW)
 
+    # TODO: Delete
     # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-    # FIXME : Implement method
+    # FIXME : Implement method, adding rectangles
     canvas.bind('<Enter>', create_rectangles)
     canvas.bind("<Leave>", disappear_rectangles)
 
-    # rectangles.append()
-    obj1_id = canvas.create_rectangle(230, 10, 290, 60, outline="#f11", fill="#1f1", width=2)
-    canvas.tag_bind(obj1_id, '<ButtonPress-1>', rectangle_on_click_event1)
-    # canvas.tag_bind(obj1_id, '<Enter>', lambda event, can=canvas, rect_id=obj1_id: rectangle_appear(can, rect_id))
-    # canvas.tag_bind(obj1_id, '<Leave>', lambda event, can=canvas, rect_id=obj1_id: rectangle_disappear(can, rect_id))
-    create_rectangle(10, 10, 2000, 1000, root, canvas, fill='green', alpha=.2)
-
-    obj2_id = canvas.create_rectangle(400, 200, 290, 60, outline="#f11", fill="#1f1", width=2)
-    canvas.tag_bind(obj2_id, '<ButtonPress-1>', rectangle_on_click_event2)
+    # rectangles.append()z
 
     # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
@@ -155,6 +124,7 @@ def create_rectangle(x1, y1, x2, y2, root, canvas, **kwargs):
     canvas.create_rectangle(x1, y1, x2, y2, **kwargs)
 
 
+# TODO: Delete
 # EVENTS
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # TODO: To implement
@@ -174,7 +144,7 @@ def rectangle_on_click_event2(event):
     print('SECOND SHAPE')
 
 
-# TODO: To implement
+# TODO: Delete
 def create_rectangles(event):
     print("on")
 
@@ -188,5 +158,5 @@ def disappear_rectangles(event):
 
 def resize_image(path):
     original_image = Image.open(path)
-    resized_image = original_image.resize((picture_width, picture_height), Image.ANTIALIAS)
-    return ImageTk.PhotoImage(resized_image)
+    # resized_image = original_image.resize((picture_width, picture_height), Image.ANTIALIAS)
+    return ImageTk.PhotoImage(original_image)
